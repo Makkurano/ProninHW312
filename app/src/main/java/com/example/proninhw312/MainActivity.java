becb81dd3c3b2dc2364729dd8dee9847e053fb76
Pronin Aleksandr <makkurano@gmail.com>
2019-06-04 21:22:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/proninhw312/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.proninhw312;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Locale locale = new Locale("ru");
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        recreate();
+    }
+
+}
